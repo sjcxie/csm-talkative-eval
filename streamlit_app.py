6fca0a233159dd74ad83d4e972a1bed1ce7627c1
SJXie <jinchenx@uw.edu>
2024-12-18 14:52:53 +0000

    commit 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,10 +85,8 @@ else:
     # automatically at the bottom of the page.
     if user_input := st.chat_input("Enter your input here."):
 
-        if st.button("Finish the conversation"):
-            #  or user_input=="SAVE" or user_input=="save" or user_input=="STOP" or user_input=="stop" or user_input =="stop" or user_input=="STOP"
-            # file_name = "{style}_P{PID}.csv".format(style=target_styles[style_id], PID=user_PID)
-            file_name = "lalala"
+        if user_input=="SAVE" or user_input=="save" or user_input=="STOP" or user_input=="stop":
+            file_name = "{style}_P{PID}.csv".format(style=target_styles[style_id], PID=user_PID)
             st.write("file name is "+file_name)
             
             chat_history_df.to_csv(file_name, index=False)
